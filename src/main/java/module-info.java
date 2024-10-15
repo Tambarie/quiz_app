138cdace7a6b1ef5931d7040b68c3da20e85397a
Emmanuel Gbaragbo <egbaragbo@gmail.com>
2024-10-15 09:01:19 -0400

    set up boilerplate for application

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.advaned_java.com_bptn_quiz_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com_bptn_quiz_app to javafx.fxml;
+    exports com_bptn_quiz_app;
+    exports com_bptn_quiz_app.controllers;
+    opens com_bptn_quiz_app.controllers to javafx.fxml;
+}
