49fa4f3e9a9500fe063fed8618f8ba1a9a2c842c
Emmanuel Gbaragbo <egbaragbo@gmail.com>
2024-10-17 22:29:23 -0400

    add login feature

--- src/main/java/com_bptn_quiz_app/controllers/LoginController.java
+++ src/main/java/com_bptn_quiz_app/controllers/LoginController.java
@@ -4,7 +4,12 @@ import com.jfoenix.controls.JFXButton;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import com_bptn_quiz_app.database.DatabaseHandler;
+import com_bptn_quiz_app.interfaces.UserAuthentication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -36,13 +41,65 @@ public class LoginController {
     void
     initialize() {
         // TODO 1. Get the inputs, 2. Create a database 3. Refactor SetScreen method to a static method
-
-        if    (loginButton != null && homeButton != null)  {
+        if (homeButton != null) {
             HomeController homeController = new HomeController();
-            homeController.SetScreen(loginButton, "/com_bptn_quiz_app/quiz_screen.fxml","Quiz App");
-            homeController.SetScreen(homeButton, "/com_bptn_quiz_app/home.fxml","Quiz App");
+            homeController.SetScreen(homeButton, "/com_bptn_quiz_app/home.fxml", "Quiz App");
         }
 
-    }
+        loginButton.setOnAction(event -> {
 
+            String email = enterEmailField.getText().trim();
+            String password = enterPasswordField.getText().trim();
+
+
+            if (email.isEmpty() || password.isEmpty()) {
+                // TODO Return an error message to the user
+                System.out.println("Enter email or password");
+                return;
+            }
+
+            UserAuthentication userAuthentication = null;
+            try {
+                userAuthentication = new DatabaseHandler();
+            } catch (SQLException | ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            ResultSet userRow = userAuthentication.getUser(email, password);
+            int counter = 0;
+            while (true){
+                try {
+                    if (!userRow.next()) break;
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+                counter++;
+            }
+            if (counter != 1) {
+                // TODO send an error message to the user
+                System.out.println("Login failed");
+                return;
+            }
+
+            System.out.println("Login successful");
+
+            loginButton.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/com_bptn_quiz_app/quiz_screen.fxml"));
+
+            try {
+                fxmlLoader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = fxmlLoader.getRoot();
+            Stage stage = new Stage();
+            stage.setTitle("Quiz App");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        });
+
+    }
 }
