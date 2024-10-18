9fddc6ffa120ca359b4d5fbdcad9e39bf1aa0e41
Emmanuel Gbaragbo <egbaragbo@gmail.com>
2024-10-17 22:31:41 -0400

    modify getUser interface

--- src/main/java/com_bptn_quiz_app/interfaces/UserAuthentication.java
+++ src/main/java/com_bptn_quiz_app/interfaces/UserAuthentication.java
@@ -1,8 +1,9 @@
 package com_bptn_quiz_app.interfaces;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public interface UserAuthentication {
-    String loginUser(String email, String password);
+    ResultSet getUser(String email, String password);
     String signUpUser(String email, String password, String firstName, String lastName) throws SQLException, ClassNotFoundException;
 }
