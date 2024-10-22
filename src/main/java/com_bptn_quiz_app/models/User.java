6f41314dacfd1be1a83bf4902e7443c28f1c685e
Emmanuel Gbaragbo <egbaragbo@gmail.com>
2024-10-22 05:56:40 -0400

    remove set methods in user class

--- src/main/java/com_bptn_quiz_app/models/User.java
+++ src/main/java/com_bptn_quiz_app/models/User.java
@@ -21,33 +21,18 @@ public class User   {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     @Override
     public String toString() {
